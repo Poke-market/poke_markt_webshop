0bc38c202389c56740c40dc5f460a6873891575e
amnabbouti <amine.abbouti@hotmail.com>
2025-02-17 05:00:07 +0100

    add ESLint configuration and dependencies; set up automated Discord notification workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// my commands:
+// npx eslint .     => to run on whole project
+// npx eslint --fix .     => to fix issues
+// npx eslint src/**/*.ts   => to lint a specific file
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import perfectionist from 'eslint-plugin-perfectionist';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  perfectionist.configs['recommended-natural'],
+  eslintConfigPrettier, // keep last
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['**/*.{js,mjs,ejs}'], // Ignore files
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      parser: tsParser, // very important: eslint needs to know i'm using typescript
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+      },
+      sourceType: 'module',
+      ecmaVersion: 'latest', // Use the latest ECMAScript features
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': [
         'warn',
-        { allowConstantExport: true },
+        { argsIgnorePattern: '^_' },
       ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'import/no-unresolved': 'error', // unresolved imports as errors
+      'import/order': [
+        'warn',
+        {
+          'newlines-between': 'always',
+        },
+      ],
+      'react/jsx-uses-react': 'off', // +17 not required
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error', // hooks rules = error
+      'react-hooks/exhaustive-deps': 'warn', // when I have deps in useEffect they should be in my array
+      'jsx-a11y/anchor-is-valid': 'warn', // href for a tags
+    },
+    settings: {
+      react: { version: 'detect' },
     },
   },
-)
+];
