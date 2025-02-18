eb33a3d09584ada55d890b38e4e61d0693b1059f
amnabbouti <amine.abbouti@hotmail.com>
2025-02-18 17:15:23 +0100

    refactor: updated ESLint configuration, improve Discord notification to target push  and pull_requests, imroved formatting, and used secret for webhook URL
    refactor: update ESLint configuration and moved workflow job name to the top of the file.
    
    - Adjusted the embed color in the Discord notification based on event type:
      - Purple for push events
      - Red for pull requests
    
    feat: enhance Discord notifications with pull request details and improved formatting
    
    fix: use secret for Discord webhook URL.

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,35 @@
-// my commands:
-// npx eslint .     => to run on whole project
-// npx eslint --fix .     => to fix issues
-// npx eslint src/**/*.ts   => to lint a specific file
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import perfectionist from 'eslint-plugin-perfectionist';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default [
+export default tseslint.config(
+  {
+    ignores: ['**/*.{js,mjs}'],
+  },
+  eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
-  perfectionist.configs['recommended-natural'],
-  eslintConfigPrettier, // keep last
   {
-    ignores: ['**/*.{js,mjs,ejs}'], // Ignore files
     languageOptions: {
-      parser: tsParser, // very important: eslint needs to know i'm using typescript
       parserOptions: {
-        project: './tsconfig.json',
+        projectService: true, // Improves performance over explicit project setting
         tsconfigRootDir: import.meta.dirname,
       },
-      sourceType: 'module',
-      ecmaVersion: 'latest', // Use the latest ECMAScript features
     },
     rules: {
-      'prettier/prettier': 'error',
+      'import/no-unresolved': 'error',
+      'import/order': ['warn', { 'newlines-between': 'always' }],
       '@typescript-eslint/no-unused-vars': [
         'warn',
         { argsIgnorePattern: '^_' },
       ],
-      '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
-      'import/no-unresolved': 'error', // unresolved imports as errors
-      'import/order': [
-        'warn',
-        {
-          'newlines-between': 'always',
-        },
-      ],
-      'react/jsx-uses-react': 'off', // +17 not required
-      'react/prop-types': 'off',
-      'react-hooks/rules-of-hooks': 'error', // hooks rules = error
-      'react-hooks/exhaustive-deps': 'warn', // when I have deps in useEffect they should be in my array
-      'jsx-a11y/anchor-is-valid': 'warn', // href for a tags
-    },
-    settings: {
-      react: { version: 'detect' },
+      'prettier/prettier': 'error',
     },
   },
-];
+  perfectionist.configs['recommended-natural'],
+  eslintConfigPrettier, // Always last to avoid conflicts
+);
