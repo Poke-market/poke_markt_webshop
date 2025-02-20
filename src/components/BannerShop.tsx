2c0a299a8950a1940a20e25746394c41f00dca56
Hannah Casier <hannah.casier@me.com>
2025-02-20 12:03:14 +0100

    add html and css

--- src/components/BannerShop.tsx
+++ src/components/BannerShop.tsx
@@ -1,4 +1,18 @@
 const BannerShop = () => {
-  return <div>BannerShop</div>;
+  return (
+    <div className="banner-shop">
+      <img
+        src="./public/pokemart.jpeg"
+        alt="Shop Banner"
+        className="banner-image"
+      ></img>
+      <h1>Shop</h1>
+      <div className="breadcrumb">
+        <span>Home</span>
+        <span> ﹥ </span>
+        <span>Shop</span>
+      </div>
+    </div>
+  );
 };
 export default BannerShop;
