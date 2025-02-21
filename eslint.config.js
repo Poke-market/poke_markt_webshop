5e7d994ed8241f6cf117baf15a6a42c946117410
amnabbouti <amine.abbouti@hotmail.com>
2025-02-21 03:11:51 +0100

    :wrench: add Prettier configuration and refactor ESLint setup.
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,28 @@
-// @ts-check
-
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import perfectionist from 'eslint-plugin-perfectionist';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  { ignores: ["dist"] },
   {
-    ignores: ['**/*.{js,mjs}'],
-  },
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
-  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      parserOptions: {
-        projectService: true, // Improves performance over explicit project setting
-        tsconfigRootDir: import.meta.dirname,
-      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
-      'import/no-unresolved': 'error',
-      'import/order': ['warn', { 'newlines-between': 'always' }],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_' },
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
       ],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'prettier/prettier': 'error',
     },
   },
-  perfectionist.configs['recommended-natural'],
-  eslintConfigPrettier, // Always last to avoid conflicts
 );
