9331bdea7a07a58ce9d70ed4ef784ae5c3499a5d
Joppe-VH <joppe.vanhoof@gmail.com>
2025-02-23 03:16:56 +0100

    use css modules to avoid further conflicts

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,16 +1,18 @@
+import styles from "../scss/components/Pagination.module.css";
+
 const Pagination = () => {
   return (
-    <div className="pagination">
-      <a href="#" className="page">
+    <div className={styles.pagination}>
+      <a href="#" className={styles.page}>
         1
       </a>
-      <a href="#" className="page">
+      <a href="#" className={styles.page}>
         2
       </a>
-      <a href="#" className="page">
+      <a href="#" className={styles.page}>
         3
       </a>
-      <a href="#" className="next">
+      <a href="#" className={styles.next}>
         Next
       </a>
     </div>
