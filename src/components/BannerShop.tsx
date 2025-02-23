116e2af38021b1ba32826a31257f8db21b723771
Joppe-VH <joppe.vanhoof@gmail.com>
2025-02-23 02:53:26 +0100

    use css modules to avoid further conflicts

--- src/components/BannerShop.tsx
+++ src/components/BannerShop.tsx
@@ -1,13 +1,15 @@
+import styles from "../scss/components/BannerShop.module.css";
+
 const BannerShop = () => {
   return (
-    <div className="banner-shop">
+    <div className={styles.bannerShop}>
       <img
         src="/pokemart.jpeg"
         alt="Shop Banner"
-        className="banner-image"
+        className={styles.bannerImage}
       ></img>
       <h1>Shop</h1>
-      <div className="breadcrumb">
+      <div className={styles.breadcrumb}>
         <span>Home</span>
         <span> ﹥ </span>
         <span>Shop</span>
