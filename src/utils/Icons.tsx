669ee254a7ceca1fcd62bae575fb981c74a084d6
amnabbouti <amine.abbouti@hotmail.com>
2025-02-28 00:49:28 +0100

    refactor: add new icons and enhance icon creation logic in Icons component
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/utils/Icons.tsx
+++ src/utils/Icons.tsx
@@ -1,8 +1,12 @@
 import { Icon } from "@iconify-icon/react";
+import { BsViewList, BsArrowRight } from "react-icons/bs";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 import { CSSProperties, ComponentType } from "react";
+
 interface IconProps {
   style?: CSSProperties;
 }
+
 const createIcon = (IconComponent: string | ComponentType<IconProps>) => {
   return ({ style }: IconProps) => {
     if (typeof IconComponent === "string") {
@@ -17,4 +21,22 @@ const createIcon = (IconComponent: string | ComponentType<IconProps>) => {
 };
 export const Icons = {
   User: createIcon("tdesign:user"),
+  Search: createIcon("iconamoon:search"),
+  Wishlist: createIcon("mynaui:heart"),
+  ShoppingCart: createIcon("hugeicons:shopping-cart-01"),
+  GridIcon: createIcon("ci:grid-big-round"),
+  Warranty: createIcon("ph:seal-check-light"),
+  Trophy: createIcon("fluent:trophy-32-regular"),
+  Delivery: createIcon("icon-park-twotone:delivery"),
+  CustomerService: createIcon("bx:support"),
+  Share: createIcon("ri:share-fill"),
+  ViewList: createIcon(BsViewList),
+  KeyBoardArrowDown: createIcon(MdKeyboardArrowDown),
+  keyboardArrowUp: createIcon(MdKeyboardArrowUp),
+  ArrowRight: createIcon(BsArrowRight),
+  XFill: createIcon("flowbite:close-circle-solid"),
+  Facebook: createIcon("formkit:facebook"),
+  Linkedin: createIcon("mdi:linkedin"),
+  XTwitter: createIcon("hugeicons:new-twitter-ellipse"),
+  ArrowRightLeft: createIcon("fluent:arrow-swap-20-filled"),
 };
