f5028af631fbc0b833fc7be4e4f80fa2e153ac1e
amnabbouti <amine.abbouti@hotmail.com>
2025-03-03 02:34:50 +0100

    feat: enhance RelatedProducts component with responsive layout and the Show More button.
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/components/RelatedProducts.tsx
+++ src/components/RelatedProducts.tsx
@@ -8,8 +8,29 @@ const RelatedProduct = () => {
         <Heading as="h2" size="text2xl">
           Related Products
         </Heading>
-        <div>Products</div>
-        <Button className="button">Show More</Button>
+        <div className={styles.relatedProducts}>
+          <div>
+            <img src="https://picsum.photos/150" alt="related product" />
+            <p>Product Name</p>
+            <p>$100</p>
+          </div>
+          <div>
+            <img src="https://picsum.photos/150" alt="related product" />
+            <p>Product Name</p>
+            <p>$100</p>
+          </div>
+          <div>
+            <img src="https://picsum.photos/150" alt="related product" />
+            <p>Product Name</p>
+            <p>$100</p>
+          </div>
+          <div>
+            <img src="https://picsum.photos/150" alt="related product" />
+            <p>Product Name</p>
+            <p>$100</p>
+          </div>
+        </div>
+        <Button className={styles.showBtn}>Show More</Button>
       </div>
     </div>
   );
