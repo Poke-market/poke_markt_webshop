768f8ca694e0f77ff453681a087eaa8e8c7e3442
amnabbouti <amine.abbouti@hotmail.com>
2025-03-04 00:25:11 +0100

    feat: add CartPage component, update App routing, set BannerShop title to "Cart"
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.tsx";
 import Homepage from "./pages/Homepage.tsx";
+import CartPage from "./pages/Cartpage.tsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/cart" element={<CartPage />} />
         </Routes>
       </Layout>
     </Router>
---
+++ src/pages/Cartpage.tsx
@@ -0,0 +1,11 @@
+import BannerShop from "../components/BannerShop";
+
+const CartPage = () => {
+  return (
+    <>
+      <BannerShop title="Cart" />
+    </>
+  );
+};
+
+export default CartPage;
