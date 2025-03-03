39f68a0c23aec756fd3a0576c6fb1e2a28ac30bd
amnabbouti <amine.abbouti@hotmail.com>
2025-03-03 01:17:22 +0100

    feat: added a detail page to display the ralated products component.
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout.tsx";
 import Homepage from "./pages/Homepage.tsx";
+import { Related } from "./utils";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/Detailpage" element={<Related />} />
         </Routes>
       </Layout>
     </Router>
---
+++ src/components/RelatedProducts.tsx
@@ -0,0 +1,13 @@
+import { Heading } from "../utils";
+
+const RelatedProduct = () => {
+  return (
+    <div>
+      <div>
+        <Heading>Related Products</Heading>
+      </div>
+    </div>
+  );
+};
+
+export default RelatedProduct;
