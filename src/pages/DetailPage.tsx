d742ba93d47a9be7f7e110c97378516964e47ef4
Hannah Casier <hannah.casier@me.com>
2025-03-04 20:56:59 +0100

    edited with heading

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ProductInfo from "../components/ProductInfo";
 import Loading from "../components/Loading";
+import Heading from "../components/Headingtxt";
 import styles from "../scss/components/DetailPage.module.scss";
 import { Product, ApiListResponse, ApiSingleResponse } from "../types/types";
 
@@ -103,11 +104,19 @@ const DetailPage = () => {
     return (
       <div className={styles.productDetail}>
         <div className={styles.notFound}>
-          <h2>Product not found</h2>
+          <Heading as="h2" size="textmd" className={styles.notFoundTitle}>
+            Product not found
+          </Heading>
           <p>Sorry, we couldn't find a product matching "{name}".</p>
           {availableProducts.length > 0 && (
             <div className={styles.availableProducts}>
-              <h3>Recommended Products:</h3>
+              <Heading
+                as="h3"
+                size="textlg"
+                className={styles.recommendationsTitle}
+              >
+                Recommended Products:
+              </Heading>
               <ul>
                 {availableProducts.map((item) => (
                   <li key={item._id}>
