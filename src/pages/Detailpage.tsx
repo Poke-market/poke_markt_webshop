259ce7873aa5fe938e908624cc9a83cb9d87b460
Hannah Casier <hannah.casier@me.com>
2025-03-05 10:18:32 +0100

    restore detailpage

--- src/pages/Detailpage.tsx
+++ src/pages/Detailpage.tsx
@@ -1,10 +1,46 @@
+import { useParams } from "react-router-dom";
 import { Related } from "../utils/index.ts";
+import ProductInfo from "../components/ProductInfo";
+import { useProduct } from "../hooks/useProduct";
+import styles from "../scss/components/DetailPage.module.scss";
 
 const Detailpage = () => {
+  const { name } = useParams();
+  const { product, loading, availableProducts } = useProduct(name);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!product) {
+    return (
+      <div className={styles.notFound}>
+        <h1 className={styles.notFoundTitle}>Product Not Found</h1>
+        <p>Sorry, we couldn't find the product you're looking for.</p>
+        <div className={styles.availableProducts}>
+          <h2 className={styles.recommendationsTitle}>Available Products</h2>
+          <ul>
+            {availableProducts.map((product) => (
+              <li key={product._id}>
+                <a
+                  href={`/product/${product.name.toLowerCase().replace(/\s+/g, "-")}`}
+                >
+                  {product.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
+      <ProductInfo product={product} />
       <Related />
     </>
   );
 };
+
 export default Detailpage;
