1479b47194457599f1e041ff2c03c572589d164c
amnabbouti <amine.abbouti@hotmail.com>
2025-03-05 01:24:03 +0100

    feat: Implemented row rendering in the Table component
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -43,7 +43,17 @@ export function Table<T>({
           </tr>
         ))}
       </thead>
-      <tbody>{}</tbody>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
