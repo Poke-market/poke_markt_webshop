afd4b2c897031b5f945e4ebee95a224023c97b6e
amnabbouti <amine.abbouti@hotmail.com>
2025-03-05 03:27:08 +0100

    feat: Replace static table with Table component
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,6 @@
-import { Button, Heading } from "../utils";
+import { Button, Heading, Img, Input } from "../utils";
+import { Table } from "./Table.tsx";
+import { createColumnHelper } from "@tanstack/react-table";
 
 type CartItem = {
   rowProduct: string;
@@ -7,59 +9,78 @@ type CartItem = {
   rowSubtotal: string;
 };
 
-const CartPage = () => (
-  <section className="cart">
-    <div className="table-container">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <div className="product">
-                <img src="https://via.placeholder.com/150" alt="product" />
-                <div className="product-info">
-                  <Heading as="h3">Product Name</Heading>
-                  <p>Product Description</p>
-                </div>
-              </div>
-            </td>
-            <td>$0.00</td>
-            <td>
-              <input type="number" value="1" readOnly />
-            </td>
-            <td>$0.00</td>
-            <td>
-              <Button>Delete</Button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div className="totals-container">
-      <div className="totals-content">
-        <Heading as="h1" size="textmd">
-          Cart Totals
-        </Heading>
-        <div className="subtotal">
-          <Heading>Subtotal</Heading>
-          <Heading>0.00</Heading>
-        </div>
-        <div className="total">
-          <Heading>Total</Heading>
-          <Heading className="total-amount">0.00</Heading>
-        </div>
-        <Button>Check Out</Button>
-      </div>
-    </div>
-  </section>
-);
+const prolongation: CartItem[] = [
+  {
+    rowProduct: "Product Name",
+    rowPrice: "0.00",
+    rowQuantity: "1",
+    rowSubtotal: "0.00",
+  },
+];
+const CartPage = () => {
+  const columnHelper = createColumnHelper<CartItem>();
 
+  const columns = [
+    columnHelper.accessor("rowProduct", {
+      cell: (info) => (
+        <div className="product-cell">
+          <div className="image-container">
+            <Img src="https://via.placeholder.com/150" alt="Product" />
+          </div>
+          <Heading>{info.getValue()}</Heading>
+        </div>
+      ),
+      header: "Product",
+    }),
+    columnHelper.accessor("rowPrice", {
+      cell: (info) => <Heading>{info.getValue()}</Heading>,
+      header: "Price",
+    }),
+    columnHelper.accessor("rowQuantity", {
+      cell: (info) => <Input type="number" defaultValue={info.getValue()} />,
+      header: "Quantity",
+    }),
+    columnHelper.accessor("rowSubtotal", {
+      cell: (info) => <Heading>{info.getValue()}</Heading>,
+      header: "Total",
+    }),
+    columnHelper.display({
+      id: "delete",
+      cell: () => <Button className="delete-cell">Delete</Button>,
+    }),
+  ];
+
+  return (
+    <section className="cart">
+      <div className="table-container">
+        <Table
+          columns={columns}
+          data={prolongation}
+          className="cart-table"
+          theadProps={{ className: "cart-thead" }}
+          thProps={{ className: "cart-th" }}
+          tbodyProps={{ className: "cart-tbody" }}
+          trProps={{ className: "cart-tr" }}
+          tdProps={{ className: "cart-td" }}
+        />
+      </div>
+      <div className="totals-container">
+        <div className="totals-content">
+          <Heading as="h1" size="textmd">
+            Cart Totals
+          </Heading>
+          <div className="subtotal">
+            <Heading>Subtotal</Heading>
+            <Heading>0.00</Heading>
+          </div>
+          <div className="total">
+            <Heading>Total</Heading>
+            <Heading className="total-amount">0.00</Heading>
+          </div>
+          <Button>Check Out</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
 export default CartPage;
