6b207876788b8f525217f13fd52337c1745389d0
Hannah Casier <hannah.casier@me.com>
2025-03-13 14:29:07 +0100

    styling loginpage

--- src/pages/Loginpage.tsx
+++ src/pages/Loginpage.tsx
@@ -1,12 +1,25 @@
-import { Footer, Header, BannerShop } from "../utils";
-
+import { BannerShop, Button, Heading, Input, UspBanner } from "../utils";
+import styles from "../scss/pages/Loginpage.module.scss";
 const Loginpage = () => {
   return (
     <>
-      <Header />
       <BannerShop title="Login" />
-      <div></div>
-      <Footer />
+      <div className={styles.loginContainer}>
+        <div>
+          <Heading as="h2" size="textlg">
+            Email address
+          </Heading>
+          <Input type="email" placeholder="Abc@def.com" />
+        </div>
+        <div>
+          <Heading as="h2" size="textlg">
+            Password
+          </Heading>
+          <Input type="password" placeholder="Password" />
+        </div>
+        <Button>Login</Button>
+      </div>
+      <UspBanner />
     </>
   );
 };
