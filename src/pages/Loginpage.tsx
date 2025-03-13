88acc0d43e49886784267a803737d3d53a8f940c
Hannah Casier <hannah.casier@me.com>
2025-03-13 13:54:58 +0100

    added a loginpage and handling on click in header to go to page

---
+++ src/pages/Loginpage.tsx
@@ -0,0 +1,13 @@
+import { Footer, Header, BannerShop } from "../utils";
+
+const Loginpage = () => {
+  return (
+    <>
+      <Header />
+      <BannerShop title="Login" />
+      <div></div>
+      <Footer />
+    </>
+  );
+};
+export default Loginpage;
