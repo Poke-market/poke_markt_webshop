eda395a628316cca197d0662a8e4d641e09c8332
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-16 01:26:37 +0100

    add PillCheckbox component

---
+++ src/components/PillCheckbox.tsx
@@ -0,0 +1,25 @@
+import { InputHTMLAttributes } from "react";
+import styles from "../scss/components/PillCheckbox.module.scss";
+import { Icons } from "../utils";
+
+type PillCheckboxProps = {
+  label: string;
+} & Omit<InputHTMLAttributes<HTMLInputElement>, "type">;
+
+const PillCheckbox = ({
+  label,
+  id = label,
+  ...inputProps
+}: PillCheckboxProps) => {
+  return (
+    <div className={styles.pillCheckbox}>
+      <input type="checkbox" id={id} {...inputProps} />
+      <label htmlFor={id}>
+        <span>{label}</span>
+        <Icons.XFill className={styles.icon} />
+      </label>
+    </div>
+  );
+};
+
+export default PillCheckbox;
