fddac482dc46a17c4d0960286489b9d9d4fb9067
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-16 05:21:33 +0100

    improve filter open/close transition

--- src/components/FilterOverlay.tsx
+++ src/components/FilterOverlay.tsx
@@ -1,6 +1,7 @@
 import { Button, Heading } from "../utils";
 import styles from "../scss/components/FilterOverlay.module.scss";
 import FilterForm from "./FilterForm";
+import clsx from "clsx";
 
 interface FilterOverlayProps {
   isOpen: boolean;
@@ -8,27 +9,27 @@ interface FilterOverlayProps {
 }
 
 export const FilterOverlay = ({ isOpen, onClose }: FilterOverlayProps) => {
-  if (!isOpen) return null;
-
   return (
     <>
-      <Button
-        className={styles.backdrop}
-        onClick={onClose}
-        onKeyDown={(e: React.KeyboardEvent<HTMLButtonElement>) =>
-          e.key === "Escape" && onClose()
-        }
-        aria-label="Close cart"
-      />
-      <div className={styles.overlay}>
-        <FilterForm />
-        <div className={styles.bottomSection}>
-          <hr />
-          <Button className={styles.closeButtonMobile} onClick={onClose}>
-            <Heading as="span" size="textxs">
-              Close
-            </Heading>
-          </Button>
+      <div className={clsx({ [styles.open]: isOpen })}>
+        <Button
+          className={styles.backdrop}
+          onClick={onClose}
+          onKeyDown={(e: React.KeyboardEvent<HTMLButtonElement>) =>
+            e.key === "Escape" && onClose()
+          }
+          aria-label="Close cart"
+        />
+        <div className={styles.overlay}>
+          <FilterForm />
+          <div className={styles.bottomSection}>
+            <hr />
+            <Button className={styles.closeButtonMobile} onClick={onClose}>
+              <Heading as="span" size="textxs">
+                Close
+              </Heading>
+            </Button>
+          </div>
         </div>
       </div>
     </>
