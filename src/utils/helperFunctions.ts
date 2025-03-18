a22208a19e33a7ac4f899c98c2232706f955eebb
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-18 02:30:53 +0100

    rework PriceRangeSlider as a controlled component

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,3 +1,5 @@
+import { BaseSyntheticEvent } from "react";
+
 export const flatMapJoin = <T>(joiner: T) => {
   return (el: T, i: number, arr: T[]) =>
     arr.length - 1 !== i ? [el, joiner] : el;
@@ -8,3 +10,18 @@ export const toggleArrayElement = <T>(array: T[], element: T) => {
     ? array.filter((e) => e !== element)
     : [...array, element];
 };
+
+/**
+ * Creates a function that prevents default event behavior before executing a callback
+ * @returns A function that prevents default and passes all arguments to the callback
+ * @example
+ * <button onClick={pd(handleClick)}>Click me</button>
+ */
+export const pd =
+  <T, E extends BaseSyntheticEvent, Args extends any[]>(
+    fn: (e: E, ...args: Args) => T,
+  ) =>
+  (e: E, ...args: Args): T => {
+    e.preventDefault();
+    return fn(e, ...args);
+  };
