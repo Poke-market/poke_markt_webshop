9ebd1c5186067cfb21221ddac86546da283fdf33
amnabbouti <amine.abbouti@hotmail.com>
2025-03-19 03:17:36 +0100

    feat: update registration form fields and add toast response handling
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/config/formFields.ts
+++ src/config/formFields.ts
@@ -1,25 +1,14 @@
-import { RegisterFormData } from "../types/apiTypes/auth";
+import { FormField } from "../components/common/Form";
 
-export type FormField = {
-  label: string;
-  type: "text" | "email" | "password" | "number";
-  name: keyof RegisterFormData;
-  required?: boolean;
-};
-
-export const formFields: FormField[] = [
+export const registerFields: FormField[] = [
   { label: "Email", type: "email", name: "email", required: true },
   { label: "Password", type: "password", name: "password", required: true },
   { label: "First Name", type: "text", name: "firstname", required: true },
   { label: "Last Name", type: "text", name: "lastname", required: true },
   { label: "City", type: "text", name: "city", required: true },
   { label: "Street", type: "text", name: "street", required: true },
-  {
-    label: "House Number",
-    type: "number",
-    name: "housenumber",
-    required: true,
-  },
+  { label: "House Number", type: "text", name: "housenumber", required: true },
   { label: "Zip Code", type: "text", name: "zipcode", required: true },
-  { label: "Telephone", type: "text", name: "telephone", required: false },
+  { label: "Telephone", type: "tel", name: "telephone", required: true },
+  { label: "Register as Admin", type: "checkbox", name: "isAdmin" },
 ];
