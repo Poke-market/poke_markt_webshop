b0d6c16e7b7a2df651302b95fb52cd9079b890cc
amnabbouti <amine.abbouti@hotmail.com>
2025-03-19 05:26:02 +0100

    feat: enhance useRegister hook to manage authentication state and handle API response
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,19 +1,30 @@
+import { useAppDispatch } from "../store";
 import { useRegisterMutation } from "../store/pokemartApi.ts";
 import { UserData } from "../types/auth.ts";
+import { setAuth } from "../store/authSlice";
+import { apiResponse } from "../types/apiTypes/response";
+
+type AuthResponseData = {
+  user?: UserData;
+  token: string;
+};
 
 export const useRegister = () => {
   const [register, { isLoading }] = useRegisterMutation();
-
+  const dispatch = useAppDispatch();
   const handleRegister = async (userData: UserData) => {
-    try {
-      const response = await register(userData).unwrap();
-      return { success: true, data: response };
-    } catch (error) {
-      return { success: false, error };
+    const response = (await register(
+      userData,
+    ).unwrap()) as apiResponse<AuthResponseData>;
+    if (response.status === "success") {
+      const { data } = response;
+      const user = data.user ?? userData;
+      const { token } = data;
+      dispatch(setAuth({ user, token }));
+      return { success: true, data };
     }
+    return { success: false, error: response };
   };
-
   return { handleRegister, isLoading };
 };
-
 export default useRegister;
