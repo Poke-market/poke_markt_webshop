b8077453efaff94ca4e201036a94b741b6773cc0
amnabbouti <amine.abbouti@hotmail.com>
2025-03-19 03:13:15 +0100

    feat: enhance Form component with dynamic field rendering and status messages
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,22 +1,164 @@
-import React, { ReactNode } from "react";
-import Button from "./Button.tsx";
+import { ChangeEvent, FormEvent, ReactNode, useEffect } from "react";
+import { Input, Button } from "./";
+import styles from "../../styles/components/common/Form.module.scss";
+import { toast } from "react-toastify";
+import { toastConfig } from "../../config";
+import {
+  ToastResponse,
+  ToastResponseType,
+  getToastResponse,
+} from "../../config/toastResponses.ts";
 
-type Props = {
-  onSubmit: (e: React.FormEvent) => void;
-  children: ReactNode;
-  isLoading: boolean;
-  submitText: string;
+export type FormField = {
+  label: string;
+  type:
+    | "text"
+    | "email"
+    | "password"
+    | "number"
+    | "checkbox"
+    | "select"
+    | "textarea"
+    | "tel";
+  name: string;
+  required?: boolean;
+  options?: { value: string; label: string }[];
+  render?: (props: {
+    name: string;
+    value: any;
+    onChange: (e: ChangeEvent<any>) => void;
+  }) => ReactNode;
+  variant?: "underline" | "fill";
+  size?: "sm" | "md" | "xs";
+  shape?: "square" | "round";
+  className?: string;
+  inputClassName?: string;
 };
 
-const Form = ({ onSubmit, children, isLoading, submitText }: Props) => {
+type FormProps<T extends Record<string, string | number | boolean>> = {
+  fields: FormField[];
+  formData: T;
+  onChange: (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>,
+  ) => void;
+  onSubmit: (e: FormEvent) => void;
+  statusMessage?: string;
+  isLoading?: boolean;
+  submitButtonText?: string;
+  buttonClassName?: string;
+  className?: string;
+  toastResponse?: { key: string; type: ToastResponseType };
+};
+
+export const Form = <T extends Record<string, string | number | boolean>>({
+  fields,
+  formData,
+  onChange,
+  onSubmit,
+  statusMessage,
+  isLoading = false,
+  submitButtonText = "Submit",
+  buttonClassName = "",
+  className = "",
+  toastResponse,
+}: FormProps<T>) => {
+  useEffect(() => {
+    if (toastResponse) {
+      const response: ToastResponse = getToastResponse(toastResponse.key);
+      const toastMethod =
+        toastResponse.type === "success" ? toast.success : toast.error;
+      toastMethod(response.message, { ...toastConfig, ...response.options });
+    }
+  }, [toastResponse]);
+
   return (
-    <form onSubmit={onSubmit}>
-      {children}
-      <Button type="submit" disabled={isLoading}>
-        {isLoading ? "Loading..." : submitText}
-      </Button>
-    </form>
+    <div className={`${styles["form-container"]} ${className}`.trim()}>
+      {statusMessage && (
+        <p
+          className={
+            statusMessage.includes("successful")
+              ? styles["success-message"]
+              : styles["error-message"]
+          }
+        >
+          {statusMessage}
+        </p>
+      )}
+
+      <form onSubmit={onSubmit}>
+        {fields.map((field) => (
+          <div
+            className={`${styles["form-group"]} ${field.className ?? ""}`.trim()}
+            key={field.name}
+          >
+            <label htmlFor={field.name}>{field.label}:</label>
+            {field.render ? (
+              field.render({
+                name: field.name,
+                value: formData[field.name as keyof T],
+                onChange,
+              })
+            ) : field.type === "select" && field.options ? (
+              <select
+                id={field.name}
+                name={field.name}
+                value={(formData[field.name as keyof T] as string) || ""}
+                onChange={onChange}
+                required={field.required}
+                className={field.inputClassName ?? ""}
+              >
+                <option value="">Select an option</option>
+                {field.options.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            ) : field.type === "textarea" ? (
+              <textarea
+                id={field.name}
+                name={field.name}
+                value={(formData[field.name as keyof T] as string) || ""}
+                onChange={onChange}
+                required={field.required}
+                className={field.inputClassName ?? ""}
+              />
+            ) : (
+              <Input
+                type={field.type}
+                name={field.name}
+                value={
+                  field.type !== "checkbox"
+                    ? (formData[field.name as keyof T] as string | number) || ""
+                    : undefined
+                }
+                checked={
+                  field.type === "checkbox"
+                    ? (formData[field.name as keyof T] as boolean)
+                    : undefined
+                }
+                onChange={
+                  onChange as (e: ChangeEvent<HTMLInputElement>) => void
+                }
+                required={field.required}
+                variant={field.variant ?? "fill"}
+                size={field.size ?? "md"}
+                shape={field.shape ?? "round"}
+                className={field.inputClassName ?? ""}
+              />
+            )}
+          </div>
+        ))}
+
+        <Button
+          type="submit"
+          disabled={isLoading}
+          className={buttonClassName}
+          cursor={isLoading ? "not-allowed" : "pointer"}
+        >
+          {isLoading ? "Processing..." : submitButtonText}
+        </Button>
+      </form>
+    </div>
   );
 };
-
-export default Form;
