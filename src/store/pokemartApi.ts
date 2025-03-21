8b279a0a9a893b68409beccaa93dc78bcf34a9d7
amnabbouti <amine.abbouti@hotmail.com>
2025-03-21 01:52:54 +0100

    feat: enhance API error handling and response transformation for items and registration
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/store/pokemartApi.ts
+++ src/store/pokemartApi.ts
@@ -1,7 +1,13 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  createApi,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
 import { getItemsData, Item, GetItemsParams } from "../types/apiTypes/item";
 import { apiResponse } from "../types/apiTypes/response";
 import { RootState } from "./index";
+import { UserData, RegisterResponse } from "../types/auth";
+import { ErrorResponse } from "../types/auth";
 
 const pokemartApi = createApi({
   reducerPath: "pokemartApi",
@@ -14,27 +20,40 @@ const pokemartApi = createApi({
       }
       return headers;
     },
-    responseHandler: async (response) => {
-      const data = (await response.json()) as apiResponse;
-      return data.data;
-    },
   }),
   endpoints: (builder) => ({
     getItems: builder.query<getItemsData, GetItemsParams>({
       query: ({ page }) => {
         return `/items?page=${page}`;
       },
+      transformResponse: (response: apiResponse): getItemsData => {
+        return response.data as getItemsData;
+      },
+      transformErrorResponse: (response: FetchBaseQueryError | ErrorResponse) =>
+        response,
     }),
     getItemById: builder.query<Item, Item["_id"]>({
       query: (id) => `/items/${id}`,
-      transformResponse: (response: { item: Item }) => response.item,
+      transformResponse: (response: apiResponse): Item => {
+        if (response.data && "item" in response.data) {
+          return response.data.item as Item;
+        }
+        throw new Error("Item not found in response");
+      },
+      transformErrorResponse: (response: FetchBaseQueryError | ErrorResponse) =>
+        response,
     }),
-    register: builder.mutation({
+    register: builder.mutation<RegisterResponse, UserData>({
       query: (userData) => ({
         url: "auth/register",
         method: "POST",
         body: userData,
       }),
+      transformResponse: (response: apiResponse): RegisterResponse => {
+        return response.data as RegisterResponse;
+      },
+      transformErrorResponse: (response: FetchBaseQueryError | ErrorResponse) =>
+        response,
     }),
   }),
 });
