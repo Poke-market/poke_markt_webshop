e6a275f8d4e1f786f8d5eb187fa12b0217bf7bc3
amnabbouti <amine.abbouti@hotmail.com>
2025-03-22 12:01:44 +0100

    feat: logout with loading overlay and success toast notification
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -1,19 +1,32 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { clearAuth } from "../../store/authSlice";
-import { Button } from "../common";
+import { Button, Loading } from "../common";
+import { getToastResponse, TOAST_KEYS, toastConfig } from "../../config";
 import { RootState } from "../../store";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import styles from "../../styles/components/Logout.module.scss";
 
 const Logout = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
   const isAuthenticated = useSelector(
     (state: RootState) => state.auth.isAuthenticated,
   );
 
-  const handleLogout = () => {
+  const handleLogout = async () => {
+    setIsLoggingOut(true);
     dispatch(clearAuth());
     localStorage.removeItem("token");
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setIsLoggingOut(false);
+
+    const { message, options } = getToastResponse(TOAST_KEYS.LOGOUT_SUCCESS);
+    toast.success(message, { ...toastConfig, ...options });
+
     void navigate("/login");
   };
 
@@ -22,9 +35,21 @@ const Logout = () => {
   };
 
   return (
-    <Button onClick={isAuthenticated ? handleLogout : handleLogin}>
-      {isAuthenticated ? "Logout" : "Login"}
-    </Button>
+    <>
+      {isLoggingOut && (
+        <div className={styles.loadingOverlay}>
+          <Loading />
+        </div>
+      )}
+      <div
+        className={styles.container}
+        style={{ "--opacity": isLoggingOut ? 0.5 : 1 } as React.CSSProperties}
+      >
+        <Button onClick={isAuthenticated ? handleLogout : handleLogin}>
+          {isAuthenticated ? "Logout" : "Login"}
+        </Button>
+      </div>
+    </>
   );
 };
 
