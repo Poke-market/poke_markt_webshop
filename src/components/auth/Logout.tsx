4c9ddabb7c1a28f2f9b330bd5ce9376a5dfbc472
amnabbouti <amine.abbouti@hotmail.com>
2025-03-22 11:49:15 +0100

    feat: Logout component conditionally render Login/Logout button based on authentication state
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -1,11 +1,15 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { clearAuth } from "../../store/authSlice.ts";
-import Button from "../common/Button.tsx";
+import { clearAuth } from "../../store/authSlice";
+import { Button } from "../common";
+import { RootState } from "../../store";
 
 const Logout = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated,
+  );
 
   const handleLogout = () => {
     dispatch(clearAuth());
@@ -13,7 +17,15 @@ const Logout = () => {
     void navigate("/login");
   };
 
-  return <Button onClick={handleLogout}>Logout</Button>;
+  const handleLogin = () => {
+    void navigate("/login");
+  };
+
+  return (
+    <Button onClick={isAuthenticated ? handleLogout : handleLogin}>
+      {isAuthenticated ? "Logout" : "Login"}
+    </Button>
+  );
 };
 
 export default Logout;
