2534494de059c8a699dcfd01aea068dff401ca3b
Hannah Casier <hannah.casier@me.com>
2025-03-23 23:45:17 +0100

    added herobanner component and stylesheet

---
+++ src/components/home/HeroBanner.tsx
@@ -0,0 +1,12 @@
+import styles from "../../styles/components/home/HeroBanner.module.scss";
+
+const HeroBanner = () => {
+  return (
+    <div className={styles.heroBanner}>
+      <div className={styles.heroBanner__content}>
+        <h1>Hero Banner</h1>
+      </div>
+    </div>
+  );
+};
+export default HeroBanner;
