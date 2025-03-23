35d034d5dce0dee4385d6f3a333f60866dd3ded2
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-23 11:08:04 +0100

    add cart item component

---
+++ src/components/cart/CartItem.tsx
@@ -0,0 +1,22 @@
+import { CartItem as CartItemType } from "../../store/cartSlice";
+import { Img } from "../common";
+import styles from "../../styles/components/cart/CartItem.module.scss";
+
+type Props = CartItemType;
+
+const CartItem = ({ item, quantity }: Props) => {
+  return (
+    <div className={styles.cartItem}>
+      <div className={styles.itemImage}>
+        <Img src={item.photoUrl} alt={item.name} />
+      </div>
+      <div className={styles.itemInfo}>
+        <h3>{item.name}</h3>
+        <p>
+          <span>{quantity}</span>X<span>&euro; {item.price}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+export default CartItem;
