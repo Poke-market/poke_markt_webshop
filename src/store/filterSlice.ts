66d0a447966dec7b5501e2da183435224e05e64d
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-23 03:51:31 +0100

    add priceRange to filterSlice

--- src/store/filterSlice.ts
+++ src/store/filterSlice.ts
@@ -1,13 +1,15 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Category } from "../types/apiTypes/item";
+import { Category, PriceRange } from "../types/apiTypes/item";
 
 interface FilterState {
   categorieCounts: Record<Category, number>;
+  priceRange: PriceRange;
   totalCount: number;
 }
 
 const initialState: FilterState = {
   categorieCounts: {} as Record<Category, number>,
+  priceRange: { min: 0, max: 1_000_000 } as PriceRange,
   totalCount: 0,
 };
 
@@ -24,15 +26,28 @@ export const filterSlice = createSlice({
     setTotalCount: (state, action: PayloadAction<number>) => {
       state.totalCount = action.payload;
     },
+    setPriceRange: (state, action: PayloadAction<PriceRange>) => {
+      if (!action.payload) return;
+
+      const { min, max } = action.payload;
+      const { min: currentMin, max: currentMax } = state.priceRange;
+
+      if (min === undefined || max === undefined) return;
+      if (min === currentMin && max === currentMax) return;
+
+      state.priceRange = { min, max };
+    },
   },
   selectors: {
     selectCategorieCounts: (state) => state.categorieCounts,
     selectTotalCount: (state) => state.totalCount,
+    selectPriceRange: (state) => state.priceRange,
   },
 });
 
-export const { setCategorieCounts, setTotalCount } = filterSlice.actions;
-export const { selectCategorieCounts, selectTotalCount } =
+export const { setCategorieCounts, setTotalCount, setPriceRange } =
+  filterSlice.actions;
+export const { selectCategorieCounts, selectTotalCount, selectPriceRange } =
   filterSlice.selectors;
 
 export default filterSlice;
