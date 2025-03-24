eead8c0250a4f9a4c818892698a38e12e461d545
Caellumn <Vincent.stoop35@gmail.com>
2025-03-24 14:28:26 +0100

    styling browse our range

--- src/components/home/browseRange.tsx
+++ src/components/home/browseRange.tsx
@@ -8,28 +8,34 @@ const browseRange = () => {
         <h2>Browse The Range</h2>
         <p>Check our Best Selection of Products</p>
       </div>
-      <section>
-        <div className="card">
-          <Link to="#">
-            <div className="imageContainer">
-              <img src="/Pokeballs.webpg" alt="Pokéball" />
+      <section className={styles.cardsContainer}>
+        <div className={styles.card}>
+          <Link to="/shop?cat=pokéballs">
+            <div className={styles.imageContainer}>
+              <img src="/Pokeballs.webp" alt="Pokéball" />
             </div>
+          </Link>
+          <Link to="/shop?cat=pokéballs">
             <h3>Gotta catch 'em all</h3>
           </Link>
         </div>
-        <div className="card">
-          <Link to="#">
-            <div className="imageContainer">
+        <div className={styles.card}>
+          <Link to="/shop?cat=medicine&cat=food&cat=berries&cat=vitamins">
+            <div className={styles.imageContainer}>
               <img src="/chansey.png" alt="Pokéball" />
             </div>
+          </Link>
+          <Link to="/shop?cat=medicine&cat=food&cat=berries&cat=vitamins">
             <h3>Care</h3>
           </Link>
         </div>
-        <div className="card">
-          <Link to="#">
-            <div className="imageContainer">
+        <div className={styles.card}>
+          <Link to="/shop?cat=evolution&cat=tm%2Fhm&cat=mega+stones">
+            <div className={styles.imageContainer}>
               <img src="/machamp.png" alt="Pokéball" />
             </div>
+          </Link>
+          <Link to="/shop?cat=evolution&cat=tm%2Fhm&cat=mega+stones">
             <h3>Enhance</h3>
           </Link>
         </div>
