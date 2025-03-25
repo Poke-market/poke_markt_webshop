855828e72a011d0b190acac4cbdc4f59b4667a2d
Joppe-VH <joppe.vanhoof@gmail.com>
2025-03-25 07:18:37 +0100

    add item and wishlist red dot

--- src/components/header/IconLinks.tsx
+++ src/components/header/IconLinks.tsx
@@ -4,7 +4,9 @@ import clsx from "clsx";
 import { headerLinks } from "../../config";
 import styles from "../../styles/components/header/Header.module.scss";
 import ProfileDropdown from "./ProfileDropdown";
-
+import { useAppSelector } from "../../store";
+import { selectCartItemCount } from "../../store/cartSlice";
+import { selectWishlistItemCount } from "../../store/wishlistSlice";
 // handlers for the icon links
 type IconLinksProps = {
   onCartClick: (e: React.MouseEvent) => void;
@@ -19,6 +21,9 @@ const IconLinks = ({
   onProfileClick,
   onWishlistClick,
 }: IconLinksProps) => {
+  const cartItemCount = useAppSelector(selectCartItemCount);
+  const wishlistItemCount = useAppSelector(selectWishlistItemCount);
+
   return (
     <div className={styles.iconContainer}>
       {/* Map through the icon links from the config */}
@@ -39,6 +44,13 @@ const IconLinks = ({
             to={link.path}
             aria-label={link.label}
             className={({ isActive }) => clsx(isActive && styles.active)}
+            data-cart-item-count={
+              link.label === "Shopping Cart"
+                ? cartItemCount || undefined
+                : link.label === "Wishlist"
+                  ? wishlistItemCount || undefined
+                  : undefined
+            }
             onClick={
               link.label === "Shopping Cart"
                 ? onCartClick
