c8169f99d7c5d7a43628d154eaa351e23baa2354
amnabbouti <amine.abbouti@hotmail.com>
2025-03-25 01:02:50 +0100

    feat: added project values and team members data
    Signed-off-by: amnabbouti <amine.abbouti@hotmail.com>

---
+++ src/config/aboutData.ts
@@ -0,0 +1,69 @@
+export const companyValues = [
+  {
+    title: "Quality First",
+    description: "We never compromise on the quality of our products.",
+    icon: "quality", // Maps to Icons.Warranty
+  },
+  {
+    title: "Innovation",
+    description: "Constantly pushing boundaries to deliver better solutions.",
+    icon: "innovation", // Maps to Icons.Settings
+  },
+  {
+    title: "Sustainability",
+    description: "Committed to eco-friendly practices at every step.",
+    icon: "sustainability", // Maps to Icons.Delivery
+  },
+  {
+    title: "Support",
+    description: "We are always here to help you, we are just a message away.",
+    icon: "Support", // Maps to Icons.Delivery
+  },
+];
+
+export const teamMembers = [
+  {
+    id: "1",
+    name: "Amine Abbouti",
+    role: "Founder & CEO",
+    bio: "Believes good design should be accessible to everyone.",
+    imageUrl: "public/amine.jpg",
+    socialLinks: {
+      twitter: "https://twitter.com/alexj",
+      linkedin: "https://linkedin.com/in/alexj",
+    },
+  },
+  {
+    id: "2",
+    name: "Vincent Stoop",
+    role: "Founder & CEO",
+    bio: "Believes good design should be accessible to everyone.",
+    imageUrl: "vinnie.png",
+    socialLinks: {
+      twitter: "https://twitter.com/samlee",
+      github: "https://github.com/samlee",
+    },
+  },
+  {
+    id: "3",
+    name: "Hannah Casier",
+    role: "Founder & CEO",
+    bio: "Believes good design should be accessible to everyone.",
+    imageUrl: "public/hannah.png",
+    socialLinks: {
+      twitter: "https://twitter.com/samlee",
+      github: "https://github.com/samlee",
+    },
+  },
+  {
+    id: "4",
+    name: "Joppe Van hoof",
+    role: "Founder & CEO",
+    bio: "Believes good design should be accessible to everyone.",
+    imageUrl: "joppe.jpg",
+    socialLinks: {
+      twitter: "https://twitter.com/samlee",
+      github: "https://github.com/samlee",
+    },
+  },
+];
